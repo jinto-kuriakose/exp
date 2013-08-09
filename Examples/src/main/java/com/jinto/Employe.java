5816dcf128e76c9d9ba459b47ca3d10d19732229
jinto-kuriakose <jinto2020@gmail.com>
2013-08-09 14:10:34 +0530

    first commit

---
+++ Examples/src/main/java/com/jinto/Employe.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.jinto;
+
+/**
+ * @author jinto
+ *
+ */
+public class Employe {
+	
+	int rollNo;
+	String name;
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
